4699e9aff5baae97b50369965b18e68afbab2913
mquesada <108757182+mquesada02@users.noreply.github.com>
2023-09-25 23:03:19 -0300

    Update vite.config.js

--- test2/vite.config.js
+++ test2/vite.config.js
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build:{
-       target: "esnext"
+       target: "esnext",
+       rollupOptions: {
+         external: [ "@supabase/supabase-js" ],
+       },
    },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 })
