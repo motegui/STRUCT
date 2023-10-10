eb59ab80d8898b0fb3028190bc0bd6fc56cf4eb5
jbogado <jbogado@itba.edu.ar>
2023-10-09 22:26:32 -0300

    modified scrape and database structure for imgs

--- scrapyProyect/myproject/spiders/promoScraper.py
+++ scrapyProyect/myproject/spiders/promoScraper.py
@@ -265,6 +265,15 @@ class promoScraper(scrapy.Spider):
                 end_date = "None"            
 
             #por ahora todo santander, luego se van a separar segun el banco
+            #imagen del descuento actual
+            imagen = promocion.xpath('./wplc:field[@id="imagenxsell"]/text()', namespaces=namespaces).get()
+            imgbanco = '/banco/contenthandler/!ut/p/digest!XzWpHd4WWNGJyUWtkUvndg/dav/fs-type1/themes/SRP9Theme/images/layout/logo/santander-mobile.png'
+            #hardcodeado
+            #hay que usar la url y strippearlo para que coincida con el formato necesario
+            url_banco = "https://www.santander.com.ar"
+            url_imgen = url_banco + imagen
+
+
             entry = {
                 'tarjeta' : promocion.xpath('./wplc:field[@id="medios"]/text()', namespaces=namespaces).getall(),
                 'local' : promocion.xpath('./wplc:field[@id="empresa"]/text()', namespaces=namespaces).get(),
@@ -275,6 +284,8 @@ class promoScraper(scrapy.Spider):
                 'valido_hasta' : "{}".format(end_date) or None,
                 'valido_desde' : "{}".format(start_date) or None,
                 'descripcion_descuento' : promocion.xpath('./wplc:field[@id="description"]/text()', namespaces=namespaces).get(),
+                'img_local':url_imgen,
+                'img_banco':'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Banco_santander_logo.svg/2560px-Banco_santander_logo.svg.png',
             }
             #se guarda cada entry en la lista de entries
             promos.append(entry)
@@ -288,13 +299,7 @@ class promoScraper(scrapy.Spider):
 
             supabase.table("LOCAL").insert(localEntry).execute()
 
-            #imagen del descuento actual
-            imagen = promocion.xpath('./wplc:field[@id="imagenxsell"]/text()', namespaces=namespaces).get()
             
-            #hardcodeado
-            #hay que usar la url y strippearlo para que coincida con el formato necesario
-            url_banco = "https://www.santander.com.ar"
-            url_imgen = url_banco + imagen
 
    
 
