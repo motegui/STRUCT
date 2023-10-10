d27596da0671c492da2f26738d616b8da61711fb
mquesada02 <mquesada@itba.edu.ar>
2023-10-09 23:03:18 -0300

    Map

--- discount-app/src/components/maps/Maps.tsx
+++ discount-app/src/components/maps/Maps.tsx
@@ -1,6 +1,8 @@
 import { Box } from '@mui/material';
-import React, { useState } from 'react';
-import Map from 'react-map-gl';
+import React, { useEffect, useRef, useState } from 'react';
+import Map, { GeolocateControl, Marker, NavigationControl, Popup } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css'; 
+import mapboxgl from 'mapbox-gl';
 
 const Maps = () => {
 
@@ -8,12 +10,32 @@ const Maps = () => {
         latitude:-34.6058270445443,
         longitude: -58.373919120610424,
         zoom: 14
-    });
+    }); 
+
+
+    const [showPopup, setShowPopup] = useState(false);
 
     return (
         <Box sx={{width: '100%', height: '100%'}}>
-            <Map initialViewState={{...viewport}} mapboxAccessToken="pk.eyJ1IjoibXF1ZXNhZGEwMiIsImEiOiJjbG45NHp5M2EwM3J0MmlvZG5ra2xjNTI1In0.vh8cLg7QO7h3gXmZ5I0ryw" style={{width: '100%', height: '100vh'}}
+            <Map initialViewState={{...viewport}} mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN ?? ''} style={{width: '100%', height: '100vh'}}
         mapStyle="mapbox://styles/mapbox/streets-v9">
+            <Marker longitude={viewport.longitude} latitude={viewport.latitude} anchor="bottom" >
+                <img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png" alt="marker" width="30px" height="30px"/>
+            </Marker>
+            <NavigationControl position='bottom-right' />
+            <GeolocateControl 
+            positionOptions={ {enableHighAccuracy: true} }
+            showUserLocation={true}
+            onGeolocate={(e) => {
+                setViewport({
+                    latitude: e.coords.latitude,
+                    longitude: e.coords.longitude,
+                    zoom: 14
+                    })}}
+              position='bottom-right'
+              trackUserLocation
+              
+            />
             </Map>
         </Box> 
         
