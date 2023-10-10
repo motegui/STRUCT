17af40daa475a00fea00967cf396134bb2c3982f
mquesada02 <mquesada@itba.edu.ar>
2023-10-10 08:27:59 -0300

    Popup

--- discount-app/src/components/maps/Maps.tsx
+++ discount-app/src/components/maps/Maps.tsx
@@ -22,6 +22,11 @@ const Maps = () => {
             <Marker longitude={viewport.longitude} latitude={viewport.latitude} anchor="bottom" >
                 <img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png" alt="marker" width="30px" height="30px"/>
             </Marker>
+            <Popup latitude={viewport.latitude} longitude={viewport.longitude} closeButton={true} closeOnClick={false} onClose={() => setShowPopup(false)} anchor="bottom" >
+               <img style={{width: '200px', height: '100px'}}src="https://lh3.googleusercontent.com/p/AF1QipOTRqYjLgGUPd_IdTDQXpCeCz7XEFGemb8HNegN=s1360-w1360-h1020"/>
+               <h3>Sede Distrito Financiero</h3>
+               <h5>San Martín 202, C1004AAB CABA</h5>
+            </Popup>
             <NavigationControl position='bottom-right' />
             <GeolocateControl 
             positionOptions={ {enableHighAccuracy: true} }
