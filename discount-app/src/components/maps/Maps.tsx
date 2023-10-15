a816ffd1f6e5b2e2cfef0a2f8acab4047dff7ba2
mquesada02 <mquesada@itba.edu.ar>
2023-10-15 16:53:33 -0300

    Geocoder

--- discount-app/src/components/maps/Maps.tsx
+++ discount-app/src/components/maps/Maps.tsx
@@ -1,13 +1,14 @@
 import { Box } from '@mui/material';
 import React, { useEffect, useRef, useState } from 'react';
 import Map, { GeolocateControl, Marker, NavigationControl, Popup } from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css'; 
-import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Geocoder from '@mapbox/mapbox-gl-geocoder'
+import GeocoderControl from './geocoder-control';
 
 const Maps = () => {
 
     const [viewport, setViewport] = useState({
-        latitude:-34.6058270445443,
+        latitude: -34.6058270445443,
         longitude: -58.373919120610424,
         zoom: 14
     }); 
@@ -16,37 +17,33 @@ const Maps = () => {
     const [showPopup, setShowPopup] = useState(false);
 
     return (
-        <Box sx={{width: '100%', height: '100%'}}>
-            <Map initialViewState={{...viewport}} mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN ?? ''} style={{width: '100%', height: '100vh'}}
-        mapStyle="mapbox://styles/mapbox/streets-v9">
-            <Marker longitude={viewport.longitude} latitude={viewport.latitude} anchor="bottom" >
-                <img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png" alt="marker" width="30px" height="30px"/>
-            </Marker>
-            <Popup latitude={viewport.latitude} longitude={viewport.longitude} closeButton={true} closeOnClick={false} onClose={() => setShowPopup(false)} anchor="bottom" >
-               <img style={{width: '200px', height: '100px'}}src="https://lh3.googleusercontent.com/p/AF1QipOTRqYjLgGUPd_IdTDQXpCeCz7XEFGemb8HNegN=s1360-w1360-h1020"/>
-               <h3>Sede Distrito Financiero</h3>
-               <h5>San Martín 202, C1004AAB CABA</h5>
-            </Popup>
-            <NavigationControl position='bottom-right' />
-            <GeolocateControl 
-            positionOptions={ {enableHighAccuracy: true} }
-            showUserLocation={true}
-            onGeolocate={(e) => {
-                setViewport({
-                    latitude: e.coords.latitude,
-                    longitude: e.coords.longitude,
-                    zoom: 14
-                    })}}
-              position='bottom-right'
-              trackUserLocation
-              
-            />
+        <Box sx={{ width: '100%', height: '100%' }}>
+            <div id = "map">
+            <Map initialViewState={{ ...viewport }} mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN ?? ''} style={{ width: '100%', height: '90vh', marginTop: '60px' }}
+                mapStyle="mapbox://styles/mapbox/streets-v9">
+                <GeocoderControl mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} position="top-left" />
+                <NavigationControl position='bottom-right' />
+                <GeolocateControl
+                    positionOptions={{ enableHighAccuracy: true }}
+                    showUserLocation={true}
+                    onGeolocate={(e) => {
+                        setViewport({
+                            latitude: e.coords.latitude,
+                            longitude: e.coords.longitude,
+                            zoom: 14
+                        })
+                    }}
+                    position='bottom-right'
+                    trackUserLocation
+
+                />
             </Map>
-        </Box> 
-        
-        
-     );
+            </div>
+        </Box>
+
+
+    );
 }
 
- 
+
 export default Maps;
