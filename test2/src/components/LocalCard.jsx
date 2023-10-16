31de6a1868114165694822eac06183bf8aed702c
kndtxt <csilvadiniz@itba.edu.ar>
2023-10-16 18:40:10 -0300

    orden alfabetico de selection

--- test2/src/components/LocalCard.jsx
+++ test2/src/components/LocalCard.jsx
@@ -14,6 +14,7 @@ function LocalCard() {
           setLocalNames([]);
         }
         setLocalNames(data.map((row) => row.id));
+        setLocalNames(localNames.slice().sort());
       };
 
     useEffect(() => {
